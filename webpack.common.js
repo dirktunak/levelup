202b2d2e850274f399c6bd15eb821c88dec07d5f
Derek Tanaka <tanaka.derek@gmail.com>
2020-03-18 17:37:05 -0400

    blank webpage

---
+++ webpack.common.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/build/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+}
